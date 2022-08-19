b43997318622fa2605a098d2835fb426dd4d4990
Samuel Bediako <samuelbediako020.com>
2022-08-19 11:39:40 -0700

    Added html css and js files

---
+++ index.js
@@ -0,0 +1,21 @@
+let toglebtn = document.querySelector("#tog");
+let ul = document.querySelector("#unorderedlist");
+
+toglebtn.addEventListener("click", function () {
+  if (toglebtn.classList.contains("tt")) {
+    toglebtn.classList.remove("tt");
+    ul.style.display = "none";
+  } else {
+    toglebtn.classList.add("tt");
+    ul.style.display = "block";
+  }
+});
+
+document.querySelectorAll(".anc").forEach(function (param) {
+  param.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
